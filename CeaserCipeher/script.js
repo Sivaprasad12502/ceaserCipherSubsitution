9aa0553f47e0393900c8818c335f6fbf1988ad63
sivaprasad <sivaprasad122003.com>
2025-01-30 21:24:30 +0530

    final

--- CeaserCipeher/script.js
+++ CeaserCipeher/script.js
@@ -1,15 +1,15 @@
-const textEle=document.getElementById('textarea')
-const btn=document.getElementById('btn')
-const result=document.querySelector('.result')
+const textEle=document.getElementById('encryptText')
+const btn=document.getElementById('encryptBtn')
+const result=document.getElementById('encryptResult')
 btn.addEventListener('click',()=>{
     let input
     input=textEle.value 
-    result.innerHTML=ceasercipther(input)
+    result.innerHTML=encryption(input)
 })
 
 
 
-ceasercipther=(word)=>{
+encryption=(word)=>{
     let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
         let result=''
@@ -28,6 +28,35 @@ ceasercipther=(word)=>{
 
 
 }
+decryption=(word)=>{
+    let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        let result=''
+        for(let i=0;i<word.length;i++){
+            let letter=word[i]
+            if(letter===' '){
+                result+=' '
+            }
+            else{
+                let index=arr.indexOf(letter)
+                let newIndex=(index-3 + arr.length)%arr.length
+                result=result+arr[newIndex]
+            }
+        }
+        return result
+
+}
+const textareaa=document.querySelector('#decryptText')
+const btnn=document.querySelector('#decryptBtn')
+const resultt=document.querySelector('#decryptResult')
+
+btnn.addEventListener('click',()=>{
+    let inputval
+    inputval=textareaa.value
+    resultt.innerHTML=decryption(inputval)
+})
+
+
 
 
 
--- CeaserCipehrCombination/script.js
+++ CeaserCipehrCombination/script.js
@@ -1,15 +1,15 @@
-const textEle=document.getElementById('textarea')
-const btn=document.getElementById('btn')
-const resultEle=document.querySelector('.result')
+const textEle=document.getElementById('encryptText')
+const btn=document.getElementById('encryptBtn')
+const resultEle=document.querySelector('#encryptResult')
 
 
 
 btn.addEventListener('click',()=>{
     let inputval
     inputval=textEle.value
-    resultEle.innerHTML=CeaserCipther(inputval)
+    resultEle.innerHTML=encryption(inputval)
 })
-CeaserCipther=(word)=>{
+encryption=(word)=>{
 
     let value = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -37,5 +37,39 @@ CeaserCipther=(word)=>{
        
 
 }
+decryption=(word)=>{
 
 
+    let key= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    let value=   [
+        'z', 'a', 'A', 'Z', 'r', 'H', 's', 'Q', 't', 'D', 'u', 'R', 'v', 'X', 'y', 'U', 'p', 'q', 'w', 'x', 'B', 'F', 'o', 'b', 'g', 'V',
+        'n', 'T', 'h', 'c', 'f', 'd', 'i', 'W', 'Y', 'j', 'k', 'S', 'P','c','e','l','m','E','I','G','J','L','K','M','N','O'
+    ];
+    let result=''   
+        for(let i=0;i<word.length;i++){
+            let letter=word[i]
+            if(letter===' '){
+                result+=' '
+            }
+            else{
+                let valueIndex=value.indexOf(letter)
+                let keyindex=key[valueIndex]
+                result=result+keyindex
+                 
+
+            }
+
+        }
+        return result
+
+}
+
+const dcrptxt=document.getElementById('decryptText')
+const dcrpbtn=document.getElementById('decryptBtn')
+const dcrprslt=document.getElementById('decryptResult')
+dcrpbtn.addEventListener('click',()=>{
+    let inputval
+    inputval=dcrptxt.value
+    dcrprslt.innerHTML=decryption(inputval)
+})
